6532f37664052e9444207e8d4873c2bc6cc7bc08
Nguyen Thai Hung <hung-nguyen@cybozu.vn>
2018-10-22 21:41:23 +0700

    Modify css

--- wp-content/themes/frido/header.php
+++ wp-content/themes/frido/header.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127399825-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-127399825-1');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 <div id="wptime-plugin-preloader"></div>
